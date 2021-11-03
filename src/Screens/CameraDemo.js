54c962ea4111033598f217cd1c8492bca17d38f6
Ali <alitrader399@gmail.com>
2021-11-03 14:41:34 +0530

    ImagePicker

---
+++ src/Screens/CameraDemo.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {View, StyleSheet, Text, Images} from 'react-native';
+
+export default class CameraDemo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photo: '',
+    };
+  }
+
+  render() {
+    return <View></View>;
+  }
+}
